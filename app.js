a1da193e04dc57078f1b34a0fbd82de1d0fea7aa
ajmain <ajmain@LAPTOP-RSN0DJMG>
2024-07-16 16:09:32 +0100

    complete task 4, get-article-id

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controller.js");
 const { getEndpoints } = require("./controllers/endpoints.controller.js");
-const { serverErrorHandler } = require("./error-handlers");
+const { getArticleById } = require("./controllers/articles.controller.js");
+const { dbErrorHandler, customErrorHandler, serverErrorHandler } = require("./error-handlers");
 
 app.get("/api/topics", getTopics);
 
-app.get('/api',getEndpoints)
+app.get('/api', getEndpoints)
 
+app.get("/api/articles/:article_id", getArticleById);
 
+app.use(dbErrorHandler);
+app.use(customErrorHandler);
 app.use(serverErrorHandler);
 
 module.exports = app;
